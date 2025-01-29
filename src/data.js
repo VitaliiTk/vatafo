1fff523d4437b0a250e339d8cd293a76b94a8175
VitaliiTk <tka4uk.gt41@gmail.com>
2025-01-29 16:52:31 +0600

    create cards data one car

--- src/data.js
+++ src/data.js
@@ -47,3 +47,35 @@ export const popularCars = [
     img: '/popular-cars/daewoo.webp'
   }
 ]
+
+export const cards = [
+  {
+    author: 'John Doe',
+    number: '+996557184916',
+    title: 'Ford F-150: 2023г. 2,7л. Автомат, Бензин',
+    category: 'auto-sale',
+    price: 5421900,
+    moneySymbol: 'KGS',
+    info: 'some info text',
+    model: 'F-150',
+    year: 2023,
+    gear: 'automatic',
+    engineVolume: 2.7,
+    stearingWheel: 'left',
+    condition: 'ideal',
+    docs: 'растаможен',
+    ride: 9760,
+    body: 'пикап',
+    fuel: 'бензин',
+    color: 'черный',
+    isAvailability: true,
+    payMethod: ['Оплата наличными', 'Расрочка'],
+    vinCode: true,
+    images: [
+      'https://media.ed.edmunds-media.com/ford/f-150/2023/oem/2023_ford_f-150_crew-cab-pickup_lariat_fq_oem_1_1280.jpg',
+      'https://www.clickheretesting.com/AshevilleFord/research/2023/f150/images/trim-2023-f150-raptor.jpg',
+      'https://www.kbb.com/wp-content/uploads/2021/10/2021-Ford-F150-Hybrid-front-right-3qtr-4x3-1.jpg',
+      'https://ddztmb1ahc6o7.cloudfront.net/oakridgeford/wp-content/uploads/2023/04/03121741/Interier-1.jpg'
+    ]
+  }
+]
